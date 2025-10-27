ade5490f489c376632e5f441d26dec2d79724444
hamzah <hamzahnafalah@gmail.com>
2025-10-27 11:23:04 +0700

    update assessment

--- src/Config/module-encoding.php
+++ src/Config/module-encoding.php
@@ -2,13 +2,13 @@
 
 return [
     "encodings" => [
-        'USER_WALLET'            => ['flag'=>'USER_WALLET','name'=>'Kode Kantong Digital Pengguna'],
-        'DEPOSIT'                => ['flag'=>'DEPOSIT','name'=>'Kode Deposit'],
-        'REFUND'                 => ['flag'=>'REFUND','name'=>'Kode Refund'],
-        'BILLING'                => ['flag'=>'BILLING','name'=>'Kode Billing'],
-        'SPLIT_PAYMENT'          => ['flag'=>'SPLIT_PAYMENT','name'=>'Kode Split Payment'],
-        'TRANSACTION'            => ['flag'=>'TRANSACTION','name'=>'Kode Transaksi'],
-        'INVOICE'                => ['flag'=>'INVOICE','name'=>'Kode Invoice'],
-        'CUSTOMER_CODE'          => ['flag'=>'CUSTOMER','name'=>'Kode Pelanggan'],
+        'USER_WALLET'            => ['label'=>'USER_WALLET','name'=>'Kode Kantong Digital Pengguna'],
+        'DEPOSIT'                => ['label'=>'DEPOSIT','name'=>'Kode Deposit'],
+        'REFUND'                 => ['label'=>'REFUND','name'=>'Kode Refund'],
+        'BILLING'                => ['label'=>'BILLING','name'=>'Kode Billing'],
+        'SPLIT_PAYMENT'          => ['label'=>'SPLIT_PAYMENT','name'=>'Kode Split Payment'],
+        'TRANSACTION'            => ['label'=>'TRANSACTION','name'=>'Kode Transaksi'],
+        'INVOICE'                => ['label'=>'INVOICE','name'=>'Kode Invoice'],
+        'CUSTOMER_CODE'          => ['label'=>'CUSTOMER','name'=>'Kode Pelanggan'],
     ],
 ];
