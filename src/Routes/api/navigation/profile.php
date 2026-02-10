e5f0c2959097f3439e749705c5e147ff56a3a371
Hamzah <hamzah@kalpahealth.com>
2026-02-10 10:08:47 +0700

    -m

--- src/Routes/api/navigation/profile.php
+++ src/Routes/api/navigation/profile.php
@@ -5,7 +5,7 @@ use Projects\Hq\Controllers\API\Navigation\Profile\ProfileController;
 use Projects\Hq\Controllers\API\Navigation\Profile\ProfilePhotoController;
 
 Route::apiResource('profile',ProfileController::class)
-    ->only(['store','show'])->parameters(['profile' => 'uuid']);
+    ->only(['store','show'])->parameters(['profile' => 'id']);
     
 Route::apiResource('profile-photo',ProfilePhotoController::class)
     ->only('store','show')->parameters(['profile-photo' => 'uuid']);
